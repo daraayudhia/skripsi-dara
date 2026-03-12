6899fa01e88ad3292325c3b4050911f12c273228
Dara <email_github_daraayudia12345@gmail.com>
2026-03-12 16:57:18 +0700

    Menampilkan kembali header Streamlit

--- Data_Pasien.py
+++ Data_Pasien.py
@@ -37,8 +37,6 @@ st.markdown("""
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
 <style>
-    /* Menghilangkan header bawaan Streamlit */
-    header {visibility: hidden;}
     
     /* Container Header Utama */
     .header-container {
